e96f0931de8f61ae55223195424254638b076305
Phoebus Giannopoulos <phoebusg@gmail.com>
2019-10-03 14:09:27 +0300

    Add files via upload

--- capskeeper_1.py
+++ capskeeper_1.py
@@ -3,45 +3,50 @@
 Created on Thu Apr 20 10:26:00 2017
 
 @author: panos
+mod for OS X PhoebusG
 """
 
 #Libraries we need
 import pyxhook
 import time
-import commands
+import subprocess
 import pyautogui
+import os
 
 #initialization
 capsstatus = 0
 capsstatusnew = 0
-capsstatus = commands.getoutput('xset q | grep LED')[65]    #THIS IS WHERE WE GET THE INITIAL CAPS LOCK STATUS    
+print("This is the current path:")
+print(os.getcwd())
+capsstatus = subprocess.run(["checkModifierKeys", "capslock"],shell=True)    #THIS IS WHERE WE GET THE INITIAL CAPS LOCK STATUS
+
 #This function is called every time a key is presssed
 def kbeventdown( event ):
     global capsstatus
-   #WHEN LEFT ALT IS PRESSED, WE CHECK THE CAPS LOCK STATUS
-    if event.Key == "Shift_L":
-        print "Shift Key Pressed!"
-        capsstatus = commands.getoutput('xset q | grep LED')[65]    #THIS IS WHERE WE GET THE CAPS LOCK STATUS    
+   #WHEN LEFT cmd IS PRESSED, WE CHECK THE CAPS LOCK STATUS
+    if event.Key == "cmd_l":
+        print ("Shift Key Pressed!")
+        capsstatus = subprocess.run(["checkModifierKeys", "capslock"],shell=True)    #THIS IS WHERE WE GET THE CAPS LOCK STATUS    
         if capsstatus == "2":
-         print "CAPS LOCK STATUS: OFF"
+         print ("CAPS LOCK STATUS: OFF")
         if capsstatus == "3":
-         print "CAPS LOCK STATUS: ON"
+         print ("CAPS LOCK STATUS: ON")
 
 #This function is called every time a key is released        
 def kbeventup( event ):
         
-    #print key info
-    #WHEN SHIFT LEFT IS RELEASED, WE CHECKED IF THE CAPS LOCK STATUS HAS CHANGED, TO TAKE THE APPROPRIATE ACTION
-    if event.Key == "Shift_L":
-        print "Shift Key Released!"
+    #print key info 
+    #WHEN cmd LEFT IS RELEASED, WE CHECKED IF THE CAPS LOCK STATUS HAS CHANGED, TO TAKE THE APPROPRIATE ACTION
+    if event.Key == "cmd_l":
+        print ("Shift Key Released!")
         time.sleep(0.1) # I PUT A SLIGHT DELAY BECAUSE MANY TIMES IT COULDNT CATCH THE CHANGE OF THE STATUS
-        capsstatusnew = commands.getoutput('xset q | grep LED')[65]     #HERE IS WHERE WE GET THE NEW STATUS   
+        capsstatusnew = subprocess.run(["checkModifierKeys", "capslock"],shell=True)     #HERE IS WHERE WE GET THE NEW STATUS   
         if capsstatusnew == capsstatus:  #HERE IS WHERE WE COMPARE THE STATUS BEFORE AND AFTER THE LANGUAGE CHANGE
-         print "CAPS LOCK STATUS DIDN'T CHANGE, I WONT DO ANYTHING!"
+         print ("CAPS LOCK STATUS DIDN'T CHANGE, I WONT DO ANYTHING!")
          
          
         else:
-         print "CAPS LOCK STATUS HAS CHANGED, SENDING CAPSLOCK KEY PRESS..."    
+         print ("CAPS LOCK STATUS HAS CHANGED, SENDING CAPSLOCK KEY PRESS...")    
          pyautogui.press('capslock')
 
 
